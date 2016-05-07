39695c4b39fa04a57d8bbe535b644fe65bb848f9
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-05-07 07:42:22 +0100

    Initial commit

---
+++ desktop/src/com/thirds/adl/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thirds.adl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thirds.adl.AppDevLanguage;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AppDevLanguage(), config);
+	}
+}
