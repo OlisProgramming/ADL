a44d9db000c3eb9c723e9d29d8f9f5cb11f8e9d9
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-05-07 09:33:37 +0100

    Smooth out logo entrance

--- core/src/com/thirds/adl/AppDevLanguage.java
+++ core/src/com/thirds/adl/AppDevLanguage.java
@@ -1,27 +1,17 @@
 package com.thirds.adl;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.*;
+import com.thirds.adl.screen.SplashScreen;
 
-public class AppDevLanguage extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class AppDevLanguage extends Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+	public void create() {
+
+        setScreen(new SplashScreen());
+    }
 }
--- desktop/src/com/thirds/adl/desktop/DesktopLauncher.java
+++ desktop/src/com/thirds/adl/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.thirds.adl.AppDevLanguage;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new AppDevLanguage(), config);
 	}
