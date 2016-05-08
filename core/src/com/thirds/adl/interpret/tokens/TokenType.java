a30ced0c24de6117859e01e0de0177f01802c219
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-05-08 12:21:33 +0100

    Add Javadoc HTML!

--- core/src/com/thirds/adl/interpret/tokens/TokenType.java
+++ core/src/com/thirds/adl/interpret/tokens/TokenType.java
@@ -2,10 +2,22 @@ package com.thirds.adl.interpret.tokens;
 
 enum TokenType {
 
-    NAME, STRING_LITERAL,
+    /** Any valid name (all characters must be letters, upper or lower case, with no other characters),
+     * e.g. <pre>projectName</pre> */
+    NAME,
+
+    /** Any string literal between double quotes, e.g. <pre>"Hello, World!"</pre> */
+    STRING_LITERAL,
+
+    /** The keyword 'text'. Used as a variable type. */
     VAR_TYPE_TEXT,
 
+    /** The variable assignment operator. NOT used as an equality operator. */
     EQUALS,
 
-    SEMICOLON, EOF
+    /** End of Statement marker. */
+    SEMICOLON,
+
+    /** End of File marker. */
+    EOF
 }
